b2c9371fe38f3786120d6dace9eac1de30130683
csignorettiunivpm <S1104984@studenti.univpm.it>
2026-06-15 13:36:43 +0200

    fine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 
-    // Room  ← versione esplicita, no $version
+    // Room versione esplicita
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation(libs.androidx.ui)
